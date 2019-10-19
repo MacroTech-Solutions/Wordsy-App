5ccfc5d5ac3d5acfbbf1727e9eb305bb579ca906
Arya Tschand <arya@voicesaver.com>
2019-10-19 00:17:34 -0400

    Saved word data locally

---
+++ Wordsy/Wordsy/SavedData.swift
@@ -0,0 +1,14 @@
+//
+//  ThreeLetters.swift
+//  Wordsy
+//
+//  Created by Arya Tschand on 10/18/19.
+//  Copyright © 2019 PEC. All rights reserved.
+//
+
+import Foundation
+
+class SavedData: Codable {
+    var ThreeWords: [String] = ["cat", "sit", "eat"]
+    var ThreeDefinitionss: [String] = ["furry animal", "put your but in a chair", "consume food"]
+}
--- Wordsy/Wordsy/ThreeVC.swift
+++ Wordsy/Wordsy/ThreeVC.swift
@@ -7,18 +7,59 @@
 //
 
 import UIKit
+import SwiftyJSON
+import FirebaseDatabase
+import ProgressHUD
 
 class ThreeVC: UIViewController {
 
-    var word = "eat"
+    var word = "testing"
     var definition = "Consume food"
     var wordArray: [[String]] = []
     var col1 = false
     var col2 = false
     var col3 = false
+    var ref = DatabaseReference()
+    var randomNumberIndex: Int = 0
     
     override func viewWillAppear(_ animated: Bool) {
+        ref = Database.database().reference()
         reset()
+    }
+    
+    /*
+    func newWord(CompletionHandler: @escaping (Bool?, Error?) -> Void){
+        do {
+            let url = NSURL(string: "https://h2grow.herokuapp.com/api")!
+            let request = NSMutableURLRequest(url: url as URL)
+            request.httpMethod = "POST"
+            
+            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.httpBody = try JSONSerialization.data(withJSONObject: [""], options: .prettyPrinted)
+            let task = URLSession.shared.dataTask(with: request as URLRequest){ data, response, error in
+                randomNumberIndex = Int.random(in: 1 ..< 4)
+                ref.child("\(randomNumberIndex)").observeSingleEvent(of: .value, with: { (snapshot) in
+                    
+                    let value = snapshot.value as? NSDictionary
+                    word = value!["Word"] as! String
+                    definition = value!["Definition" ] as! String
+                    CompletionHandler(true,nil)
+                    
+                })
+                
+            }
+            task.resume()
+        } catch {
+            print(error)
+        }
+    } */
+    
+    func newWord() {
+        var data = SavedData()
+        randomNumberIndex = Int.random(in: 1 ..< data.ThreeWords.count)
+        word = data.ThreeWords[randomNumberIndex]
+        definition = data.ThreeDefinitionss[randomNumberIndex]
+        
     }
     
     @IBOutlet weak var DefinitionLabel: UILabel!
@@ -138,6 +179,7 @@ class ThreeVC: UIViewController {
             col3 = false
             let selectedWord = getWord()
             if selectedWord == word {
+                ProgressHUD.showSuccess()
                 if Button1.backgroundColor == UIColor.orange {
                     Button1.backgroundColor = UIColor.green
                 }
@@ -167,6 +209,7 @@ class ThreeVC: UIViewController {
                 }
                 reset()
             } else  {
+                ProgressHUD.showError()
                 if Button1.backgroundColor == UIColor.orange {
                     Button1.backgroundColor = UIColor.red
                 }
@@ -200,13 +243,79 @@ class ThreeVC: UIViewController {
     }
     
     func reset() {
+        /*
+        newWord() {
+            (returnval, error) in
+            if (returnval)!
+            {
+                DispatchQueue.main.async {
+                    DefinitionLabel.text = definition
+                    let index1 = Int.random(in: 0 ..< 3)
+                    let index2 = Int.random(in: 0 ..< 3)
+                    let index3 = Int.random(in: 0 ..< 3)
+                    var characterArray: [String] = []
+                    let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
+                    let emptyArray = ["", "", ""]
+                    wordArray = []
+                    wordArray.append(emptyArray)
+                    wordArray.append(emptyArray)
+                    wordArray.append(emptyArray)
+                    wordArray[0][index1] = String(word[0])
+                    wordArray[1][index2] = String(word[1])
+                    wordArray[2][index3] = String(word[2])
+                    for x in 0...5 {
+                        var finished = false
+                        while finished == false {
+                            let random = Int.random(in: 0 ..< 26)
+                            if letters[random] != String(word[x/2]) {
+                                characterArray.append(letters[random])
+                                finished = true
+                            }
+                        }
+                    }
+                    var counter = 0
+                    for x in 0...8 {
+                        if wordArray[x/3][x%3] == "" {
+                            wordArray[x/3][x%3] = characterArray[counter]
+                            counter += 1
+                        }
+                    }
+                    Button1.setTitle(wordArray[0][0], for: .normal)
+                    Button2.setTitle(wordArray[1][0], for: .normal)
+                    Button3.setTitle(wordArray[2][0], for: .normal)
+                    Button4.setTitle(wordArray[0][1], for: .normal)
+                    Button5.setTitle(wordArray[1][1], for: .normal)
+                    Button6.setTitle(wordArray[2][1], for: .normal)
+                    Button7.setTitle(wordArray[0][2], for: .normal)
+                    Button8.setTitle(wordArray[1][2], for: .normal)
+                    Button9.setTitle(wordArray[2][2], for: .normal)
+                    Button1.backgroundColor = UIColor.blue
+                    Button2.backgroundColor = UIColor.blue
+                    Button3.backgroundColor = UIColor.blue
+                    Button4.backgroundColor = UIColor.blue
+                    Button5.backgroundColor = UIColor.blue
+                    Button6.backgroundColor = UIColor.blue
+                    Button7.backgroundColor = UIColor.blue
+                    Button8.backgroundColor = UIColor.blue
+                    Button9.backgroundColor = UIColor.blue
+                }
+            } else {
+                print(error)
+            }
+        }
+        DispatchQueue.main.async { // Correct
+        }
+        
+    }
+ */
+    newWord()
         DefinitionLabel.text = definition
         let index1 = Int.random(in: 0 ..< 3)
         let index2 = Int.random(in: 0 ..< 3)
         let index3 = Int.random(in: 0 ..< 3)
         var characterArray: [String] = []
-        var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
-        var emptyArray = ["", "", ""]
+        let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
+        let emptyArray = ["", "", ""]
         wordArray = []
         wordArray.append(emptyArray)
         wordArray.append(emptyArray)
@@ -214,7 +323,7 @@ class ThreeVC: UIViewController {
         wordArray[0][index1] = String(word[0])
         wordArray[1][index2] = String(word[1])
         wordArray[2][index3] = String(word[2])
-        for var x in 0...5 {
+        for x in 0...5 {
             var finished = false
             while finished == false {
                 let random = Int.random(in: 0 ..< 26)
@@ -225,7 +334,7 @@ class ThreeVC: UIViewController {
             }
         }
         var counter = 0
-        for var x in 0...8 {
+        for x in 0...8 {
             if wordArray[x/3][x%3] == "" {
                 wordArray[x/3][x%3] = characterArray[counter]
                 counter += 1
@@ -250,8 +359,6 @@ class ThreeVC: UIViewController {
         Button8.backgroundColor = UIColor.blue
         Button9.backgroundColor = UIColor.blue
     }
-    
-
 }
 
 extension String {
